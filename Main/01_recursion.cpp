59e6ee482dfda0434d8bf6fe5ee0cc1a64cd4e8c
Zvonimir Pervan <z.pervan65@gmail.com>
2020-06-29 13:20:12 +0200

    Basic recursion exercise

---
+++ Main/01_recursion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void function(int n)
+{
+    if(n > 0)
+    {
+        function(n-1);
+        std::cout << "N: " << n << std::endl;
+    }
+}
+
+int main()
+{
+    int x = 3;
+
+    function(x);
+    return 0;
+}
