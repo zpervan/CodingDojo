4f4511c2332b800118433af590cf78f897e8a9fe
Zvonimir Pervan <z.pervan65@gmail.com>
2021-05-13 12:23:43 +0200

    testingGrounds: Basic variadic template example

--- TestingGround/testing_ground.cpp
+++ TestingGround/testing_ground.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-int main()
-{
-  std::cout << "Hi!" << std::endl;
+template <class T> void PrintThisMan(T value) {
+  std::cout << "I'm printing " << value << "\n";
+}
+
+template <typename T> T adder(T v) { return v; }
+
+template <typename T, typename... Args> T adder(T first, Args... args) {
+  return first + adder(args...);
+}
+
+int main() {
+  PrintThisMan(10);
+  PrintThisMan("Hello boi");
+  PrintThisMan(10.5);
+
+  PrintThisMan(adder(1, 2, 3, 4, 5, 6, 7));
+
   return 0;
 }
