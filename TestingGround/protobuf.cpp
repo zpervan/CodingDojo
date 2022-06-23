00e47ab134cb5703b9cf19ad988f3c167031c6f3
Zvonimir Pervan <z.pervan65@gmail.com>
2022-06-23 19:33:16 +0200

    Add protobuf example

---
+++ TestingGround/protobuf.cpp
@@ -0,0 +1,11 @@
+#include "TestingGround/proto_lib/example.pb.h"
+
+int main()
+{
+    CodingDojo::Person person;
+    person.set_name("Zvonimir");
+    person.set_email("crazy_email");
+    person.set_id(1);
+
+    return 0;
+}
